a19cf07bbf7a8c27c27df2c7ca87dd9d4816aacf
yunyu950908 <yunyu950908@gmail.com>
2019-02-22 13:07:48 +0800

    + fibonacci_loop

--- GrokkingAlgorithms/tail_call_recursion.py
+++ GrokkingAlgorithms/tail_call_recursion.py
@@ -10,7 +10,15 @@ def fibonacci_tail_call(n, prev=1, curr=1):
     return fibonacci_tail_call(n-1, curr, prev+curr)
 
 
+def fibonacci_loop(n):
+    prev, curr = 0, 1
+    for _ in range(n):
+        prev, curr = curr, prev+curr
+    return curr
+
+
 if __name__ == "__main__":
     import timeit
     print(timeit.timeit("fibonacci(10)", "from __main__ import fibonacci"))
     print(timeit.timeit("fibonacci_tail_call(10)","from __main__ import fibonacci_tail_call"))
+    print(timeit.timeit("fibonacci_loop(10)","from __main__ import fibonacci_loop"))
